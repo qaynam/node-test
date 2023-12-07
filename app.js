9dcc490d5026d7047974f226a531dbb945555d6e
qaynam <hebibullajan@gmail.com>
2023-12-07 11:31:37 +0900

    update port

--- app.js
+++ app.js
@@ -5,6 +5,6 @@ const app = express();
 
 app.use(route)
 
-app.listen(process.env.PORT || 3000, () => {
+app.listen(process.env.PORT || 30030, () => {
   console.log("Server is running on port 3000");
 });
